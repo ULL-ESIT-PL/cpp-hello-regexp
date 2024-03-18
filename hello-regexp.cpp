ae0bd34855e6ad49e2fd2c4c32b4ec788537136c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-03-18 13:51:45 +0000

    regexps-in-cpp

--- hello-regexp.cpp
+++ hello-regexp.cpp
@@ -3,10 +3,12 @@
 
 int main() {
     std::string input = "Hello, world of dynamic C++ regexps!";
-    std::regex pattern("w.*d");
+    std::regex pattern("w(.*?)d"); // Regular expression with capturing groups and lazy quantifier
+    std::smatch matches;
 
-    if (std::regex_search(input, pattern)) {
+    if (std::regex_search(input, matches, pattern)) {
         std::cout << "Found 'world' in the input string." << std::endl;
+        std::cout << "First captured group: '" << matches[1] << "'" << std::endl;
     } else {
         std::cout << "Didn't find 'world' in the input string." << std::endl;
     }
